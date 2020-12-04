e900cb3553d730c89eccc7cc425639f373492cb4
Brianna Hebeler <briannahebeler@gmail.com>
2020-12-04 00:22:58 -0500

    added seeds for employee table

--- db/schema.sql
+++ db/schema.sql
@@ -30,9 +30,9 @@ DROP TABLE IF EXISTS employee;
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     --to hold employee first name--
-    first_name VARCHAR(30),
+    first_name VARCHAR(30) NOT NULL,
     --to hold employee last name--
-    last_name VARCHAR(30),
+    last_name VARCHAR(30) NOT NULL,
     --to hold reference to role employee has--
     role_id INT,
     --to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager--
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,5 +4,3 @@ VALUES ("Engineering"), ("Accounting"), ("Sales");
 INSERT INTO role (title, salary, department_id)
 VALUES ("Jr. Engineer", 60000, 1), ("Engineer", 80000, 1), ("Sr. Engineer", 100000, 1), ("Director of Engineering", 125000, 1), ("Jr. Accountant", 60000, 2), ("Accountant", 80000, 2), ("Sr. Accountant", 100000, 2), ("Director of Accounting", 125000, 2), ("Jr. Salesman", 60000, 3), ("Salesman", 80000, 3), ("Sr. Salesman", 100000, 3), ("Director of Sales", 125000, 3);
 
-
-INSERT INTO employee
